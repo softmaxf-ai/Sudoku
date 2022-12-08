7fec080124105c10406646038c0ab9f4c2ee2424
Justwanttolearnabit <maxleif1@web.de>
2022-12-08 21:21:47 +0100

    Added function for setting up a viable Sudoku Solution and added requirements.txt

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+"""This project is a classic Sudoku Game,
+    - Random configure a Sudoku Puzzle
+    - Give Puzzle to an optimizer
+    - Optimizer Solves Puzzle """
+from random import sample
+import numpy as np
 
 
-# Press the green button in the gutter to run the script.
+class SudokuRiddler():
+    def __init__(self, sudoku_base):
+        self.solution = None
+        self.sudoku_base = sudoku_base
+
+    def setup_sudoku(self):
+        # call Solution function
+        self.setup_solution()
+
+    def setup_solution(self):
+        """ Source https://stackoverflow.com/questions/45471152/how-to-create-a-sudoku-puzzle-in-python """
+        side = self.sudoku_base**2
+
+        # pattern for a sudoku_baseline valid solution
+        def pattern(r, c): return (sudoku_base * (r % sudoku_base) + r // sudoku_base + c) % side
+        # randomize rows, columns and numbers (of valid sudoku_base pattern)
+        def shuffle(s): return sample(s, len(s))
+
+        rSudoku_base = range(sudoku_base)
+        rows = [g * sudoku_base + r for g in shuffle(rSudoku_base) for r in shuffle(rSudoku_base)]
+        cols = [g * sudoku_base + c for g in shuffle(rSudoku_base) for c in shuffle(rSudoku_base)]
+        nums = shuffle(range(1, sudoku_base * sudoku_base + 1))
+
+        # produce board using randomized sudoku_baseline pattern
+        solution = [[nums[pattern(r, c)] for c in cols] for r in rows]
+        self.solution = np.array(solution)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    sudoku_base = 3
+    puzzler = SudokuRiddler(sudoku_base)
+    puzzler.setup_sudoku()
